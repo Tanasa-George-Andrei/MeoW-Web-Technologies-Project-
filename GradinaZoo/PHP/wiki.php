e1512b5d6ca96fbd278cb38c4bb9c1248ad2b8fc
Tanasa-George-Andrei <79099043+Tanasa-George-Andrei@users.noreply.github.com>
2022-04-10 02:33:16 +0300

    Finished Wiki Page Template. Working On Search Page

---
+++ GradinaZoo/PHP/wiki.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Wiki Entry</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Wiki entry">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../CSS/wiki.css">
+    </head>
+
+    <body>
+        <?php include "../HTML/navbar.html"?>
+        <div id="info-block-container">
+            <div id="info-block">
+
+                <div id="animal-profile">
+
+                    <img src="../Img/temp.jpg" alt="Temporary image of a parrot">
+                    <p><b>Temporary Parrot</b></p>
+                    <p><i>(Temporalis Psittaciformes)</i></p>
+
+                </div>
+
+                <div id="animal-text">
+                    <h1>
+                        Lorem ipsum dolor
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo orci sollicitudin sem consectetur euismod. Fusce fermentum augue et elit dictum, quis egestas justo consequat. Suspendisse porttitor aliquam lectus, varius consectetur sapien venenatis in. Pellentesque in egestas est, vitae tincidunt ex. Praesent at metus aliquet, tincidunt nunc vel, vulputate diam. Vestibulum lobortis nulla velit, et egestas justo bibendum eleifend. Vivamus sit amet pulvinar ante, nec sagittis nisi. Cras ullamcorper non augue in sollicitudin. Vivamus varius nisl diam, eu pretium sem convallis sit amet. Curabitur vestibulum tincidunt diam id aliquet. Cras turpis ex, aliquam sed felis vel, convallis bibendum orci. Aliquam erat volutpat. Proin eget tempor ante. 
+                    </p>
+                    <h1>
+                        Lorem ipsum dolor
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo orci sollicitudin sem consectetur euismod. Fusce fermentum augue et elit dictum, quis egestas justo consequat. Suspendisse porttitor aliquam lectus, varius consectetur sapien venenatis in. Pellentesque in egestas est, vitae tincidunt ex. Praesent at metus aliquet, tincidunt nunc vel, vulputate diam. Vestibulum lobortis nulla velit, et egestas justo bibendum eleifend. Vivamus sit amet pulvinar ante, nec sagittis nisi. Cras ullamcorper non augue in sollicitudin. Vivamus varius nisl diam, eu pretium sem convallis sit amet. Curabitur vestibulum tincidunt diam id aliquet. Cras turpis ex, aliquam sed felis vel, convallis bibendum orci. Aliquam erat volutpat. Proin eget tempor ante. 
+                    </p>
+                    <h1>
+                        Lorem ipsum dolor
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo orci sollicitudin sem consectetur euismod. Fusce fermentum augue et elit dictum, quis egestas justo consequat. Suspendisse porttitor aliquam lectus, varius consectetur sapien venenatis in. Pellentesque in egestas est, vitae tincidunt ex. Praesent at metus aliquet, tincidunt nunc vel, vulputate diam. Vestibulum lobortis nulla velit, et egestas justo bibendum eleifend. Vivamus sit amet pulvinar ante, nec sagittis nisi. Cras ullamcorper non augue in sollicitudin. Vivamus varius nisl diam, eu pretium sem convallis sit amet. Curabitur vestibulum tincidunt diam id aliquet. Cras turpis ex, aliquam sed felis vel, convallis bibendum orci. Aliquam erat volutpat. Proin eget tempor ante. 
+                    </p>
+                    <h1>
+                        Lorem ipsum dolor
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo orci sollicitudin sem consectetur euismod. Fusce fermentum augue et elit dictum, quis egestas justo consequat. Suspendisse porttitor aliquam lectus, varius consectetur sapien venenatis in. Pellentesque in egestas est, vitae tincidunt ex. Praesent at metus aliquet, tincidunt nunc vel, vulputate diam. Vestibulum lobortis nulla velit, et egestas justo bibendum eleifend. Vivamus sit amet pulvinar ante, nec sagittis nisi. Cras ullamcorper non augue in sollicitudin. Vivamus varius nisl diam, eu pretium sem convallis sit amet. Curabitur vestibulum tincidunt diam id aliquet. Cras turpis ex, aliquam sed felis vel, convallis bibendum orci. Aliquam erat volutpat. Proin eget tempor ante. 
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
