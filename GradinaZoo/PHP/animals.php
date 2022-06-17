bfc44f5e799710b14e660440d59b747acb9a8d6f
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-06-17 22:24:37 +0300

    Add php
    Am facut modificari la paginile de about, animals si contact

---
+++ GradinaZoo/PHP/animals.php
@@ -0,0 +1,145 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="animals.css">
+    <title>Animals</title>
+</head>
+
+<body>
+    <div class="banner">
+            <div class="navbar">
+                <ul>
+                    <strong><li><a href="welcome.html">Home</a></li></strong>
+                    <strong><li><a href="review.html">Review</a></li></strong>
+                    <strong><li><a href="animals.html">Animals</a></li></strong>
+                    <strong><li><a href="about.php">About</a></li></strong>
+                    <strong><li><a href="animals.php">Animals</a></li></strong>
+                    <strong><li><a href="contact.php">Contact Us</a></li></strong>
+                    <strong><li><a href="login.html">LogIn</a></li></strong>
+                    <strong><li><a href="">Logout</a></li></strong>
+                    <strong><li><a href="raport.html">Raport</a></li></strong>
+                </ul>
+            </div>
+            
+         <br>
+         <br>
+         <br>
+    <div class="grid">
+        <div class="grid-item">
+        <div class="card">
+          <img class="card-img" src="mammals.jpg" alt="Mammals" />
+          <div class="card-content">
+            <h1 class="card-header">Mammals</h1>
+            <p class="card-text">
+            Mammals are part of the class of vertebrate animals that give birth to live chicks. 
+            They live in all habitats and total over 5,800 species spread around the globe and 
+            150 of them in our zoo garden.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="card">
+          <img
+            class="card-img"
+            src="reptiles.jpg"
+            alt="Reptiles"
+          />
+          <div class="card-content">
+            <h1 class="card-header">Reptiles</h1>
+            <p class="card-text">
+            Reptiles are a class of vertebrates with their bodies covered in thick, 
+            scaly, legless, or short-legged skin. They are divided into about 7000 species, 
+            of which 200 in our zoo garden.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="card">
+          <img class="card-img" src="fish.jpg" alt="Fish" />
+          <div class="card-content">
+            <h1 class="card-header">Fish</h1>
+            <p class="card-text">
+            Fish are part of the class of vertebrate, skulled animals that live in the aquatic environment.
+            They breathe oxygen dissolved in water and total 29,000 species of fish, of which 100 species in our zoo garden.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid-item">
+        <div class="card">
+          <img class="card-img" src="birds.jpg" alt="Birds" />
+          <div class="card-content">
+            <h1 class="card-header">Birds</h1>
+            <p class="card-text">
+            Birds are a group of warm-blooded vertebrates characterized by toothless 
+            feathers and beaks. They number over 1200 species, of which 95 in our zoo garden.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+</body>
+</html>
+
+
+<!-- 
+
+
+     <div class="grid">
+      <div class="grid-item">
+        <div class="card">
+          <img class="card-img" src="./img/rome.jpg" alt="Rome" />
+          <div class="card-content">
+            <h1 class="card-header">Rome</h1>
+            <p class="card-text">
+              Rome is known for its stunning <strong> architecture</strong>,
+              with the Colleseum, Pantheon, and Trevi Fountain as the main
+              attractions.
+            </p>
+            <button class="card-btn">Visit <span>&rarr;</span></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="card">
+          <img
+            class="card-img"
+            src="./img/grand-canyon.jpg"
+            alt="Grand Canyon"
+          />
+          <div class="card-content">
+            <h1 class="card-header">Grand Canyon</h1>
+            <p class="card-text">
+              One of the world's natural wonders, the iconic Grand Canyon draws
+              oohs and aahs from visitors perched at the edge of its
+              <strong>towering cliffs</strong>.
+            </p>
+            <button class="card-btn">Visit <span>&rarr;</span></button>
+          </div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="card">
+          <img class="card-img" src="./img/maldives.jpg" alt="Maldives" />
+          <div class="card-content">
+            <h1 class="card-header">Maldives</h1>
+            <p class="card-text">
+              The Maldives are known for their
+              <strong>natural environment</strong> including the blue ocean,
+              white beaches, and clean air, attracting tourists.
+            </p>
+            <button class="card-btn">Visit <span>&rarr;</span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+ -->
