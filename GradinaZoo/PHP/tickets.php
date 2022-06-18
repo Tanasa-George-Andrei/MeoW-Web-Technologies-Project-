23089b819fbb10221b02aeb969b542ad73eafe94
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-06-18 17:32:15 +0300

    Modificari pagina tickets
    Am facut modificari la fisierele tickets.php si tickets.css

--- GradinaZoo/PHP/tickets.php
+++ GradinaZoo/PHP/tickets.php
@@ -23,7 +23,7 @@ if (isset($_POST["date"])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="tickets.css">
+  <link rel="stylesheet" href="../CSS/tickets.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&amp;display=swap" rel="stylesheet">
   <title>Tickets</title>
 </head>
