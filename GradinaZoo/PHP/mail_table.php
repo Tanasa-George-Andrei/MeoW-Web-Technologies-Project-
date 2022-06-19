bb0ac302e41ae0f8534ffbb97c10ddcac64d5362
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-06-20 02:44:50 +0300

    Add mail_table.php si ticket_table.php
    Am adaugat mail_table.php si ticket_table.php

---
+++ GradinaZoo/PHP/mail_table.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mail_table.css">
+    <title>Mail Table</title>
+</head>
+
+<body>
+<table>
+<tr>
+<th>Name</th>
+<th>Email</th>
+<th>Message</th>
+</tr>
+
+<?php
+$conn = mysqli_connect("localhost", "user", "password", "zoo");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM mails";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["res_name"]. "</td><td>" . $row["res_email"] . "</td><td>"
+. $row["res_message"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+
+</table>
+</body>
+</html>
