d38fb17f4b8e50d17d45cbf60cf68fb913d30b65
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-06-20 10:38:36 +0300

    Update Review.php
    update

--- GradinaZoo/PHP/Review.php
+++ GradinaZoo/PHP/Review.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../CSS/review.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <title>Home</title>
     </head>
 
